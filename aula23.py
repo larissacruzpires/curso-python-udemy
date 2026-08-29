5667c2f650edf2599ae366534eee4a3adda93e38
Larissa Cruz <laricruzpires@gmail.com>
2026-08-28 21:43:54 -0300

    Aula 23 - Tipos de Dados Primitivos - Inteiro - fundamentos de Python

---
+++ aula23.py
@@ -0,0 +1,25 @@
+TIPO INTEIRO:
+------------------------------------------
+10
+num  = 10
+print(num)
+10+20
+7/2
+int(7/2)
+7//2
+7%2
+7**2
+type(10)
+type(num)
+#tamanho: 32 ou 64 bits?  Em python não tem limite!!
+2**32
+2**64
+2**2000
+#para limpar o console: CRTL + L
+1000000  -->  1_000_000
+#PARA FACILITAR A VISUALIZAÇÃO
+num = num+1
+num+=1
+#(-, *, /)
+dir(num)
+num.__add__(8)
