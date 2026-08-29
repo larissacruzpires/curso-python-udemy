8aacdf07e2f5ed3ca663e5bfddd17f6ee3d1f350
Larissa Cruz <laricruzpires@gmail.com>
2026-08-28 21:45:36 -0300

    Aula 25 - Tipos de Dados Primitivos - Booleano (Lógico) - fundamentos de Python

---
+++ aula25.py
@@ -0,0 +1,18 @@
+TIPO BOOLEANO (LÓGICO)
+ATENÇÃO: True    False
+------------------------------------------
+True
+flag  = False
+print(flag)
+
+type(flag)
+
+dir(flag)
+flag.real
+
+flag = False
+flag.real
+
+num = 2
+dir(num)
+nu
