523dd22e8c706473d6b1ec46e1b5d9f5b6243c21
Larissa Cruz <laricruzpires@gmail.com>
2026-08-28 21:44:47 -0300

    Aula 24 - Tipos de Dados Primitivos - Float - fundamentos de Python

---
+++ aula24.py
@@ -0,0 +1,38 @@
+TIPO FLOAT (reais ou decimais):
+ATENÇÃO: O separador de casas decimais 
+é o PONTO e não a VIRGULA
+------------------------------------------
+10.0
+num  = 10.0
+print(num)
+10.44 + 20.56
+7/2
+int(7/2)
+inteiro = 5
+num = float(inteiro)
+print(num)
+type(10.44)
+type(num)
+#tamanho: 64 bits?  Em python não tem limite!!
+2.45**32
+2.45**64
+2.45**2000
+#para limpar o console: CRTL + L
+1000000.00000000  -->  1_000_000.000_000_00
+#PARA FACILITAR A VISUALIZAÇÃO
+
+num = num+1.8
+print(int(num))
+
+# cuidado com a perda da precisão no processo de conversão
+dir(num)
+num.__add__(8)
+
+#Numeros complexos
+# n acompanhado de i   //  n acompanhado de j
+5j
+6+5j
+5*6j
+5**6j
+
+
