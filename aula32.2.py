896cf3f7a01527136e6226f4723eb8ccb533a66a
Larissa Cruz <laricruzpires@gmail.com>
2026-08-29 21:31:18 -0300

    Aula 32.2 - Comando elif: Valores de frutas - Fundamentos de Python

---
+++ aula32.2.py
@@ -0,0 +1,11 @@
+# Comando elif (controle de decisão multidirecional): Valores das frutas
+
+fruta = input("Digite a fruta: ")
+if (fruta == 'Banana'):
+    print("O kg da Banana é 5,23")
+elif (fruta == 'Maça'):
+    print("O kg da Maça é 12,10")
+elif (fruta == 'Cereja'):
+    print("O kg da Cereja é 58,00")
+else:
+    print("Desculpe. Não temos ", fruta)
