1b37e76f42bf9a33a9e1561c0f600aad4123683c
Larissa Cruz <laricruzpires@gmail.com>
2026-08-29 21:27:53 -0300

    Aula 31 - Estrutura Condicional: Idade - Fundamentos de Python

---
+++ aula31.py
@@ -0,0 +1,7 @@
+# Estrututa Condicional: Idade
+
+idade = int(input("Qual a sua idade? "))
+if (idade>=18):
+    print("Parabéns! Você já é maior de idade ")
+else:
+    print("Espere mais um pouco! Logo você será maior de idade")
