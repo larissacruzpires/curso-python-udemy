070dc1872c62845e305a00ab04d4d5c80f9304e4
Larissa Cruz <laricruzpires@gmail.com>
2026-08-31 06:57:19 -0300

    Primeiro Desafio - Aula 34

--- nível 1/operadores e condicionais/desafio1_condicionais.py
+++ nível 1/operadores e condicionais/desafio1_condicionais.py
@@ -1,7 +1,7 @@
 # Estrutura Condicional: Primeiro Desafio
 # O Número é par ou ímpar?
 
-n = int(input("Digite um número inteiro e positivo: "))
+n = int(input("Digite um número inteiro e positivo!: "))
 if n > 0:
     if (n % 2) == 0:
         print(n, " é um número par!")
