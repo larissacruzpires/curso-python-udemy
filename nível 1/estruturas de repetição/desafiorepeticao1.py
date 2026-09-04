3d6eede6b001c7bcb3d533458f4cf526ea6f2c88
Larissa Cruz <laricruzpires@gmail.com>
2026-09-04 09:57:39 -0300

    Primeiro Desafio - Aula 40

--- nível 1/estruturas de repetição/desafiorepeticao1.py
+++ nível 1/estruturas de repetição/desafiorepeticao1.py
@@ -1,9 +1,9 @@
 # Desafio de Estrutas de Repetição - WHILE
 # Exercício prátrico sobre loops
 
-soma = 0
 n = int(input("Digite um número: "))
+soma = 0
 while n != 0:
     soma = soma + (n % 10)
     n = (n // 10)
-print(f"Resultado da soma entre estes números é {soma}.")
+print(f"A soma entre estes números é {soma}.")
