1b309caf054c6076a8743c41a22bae104b8c12b6
Larissa Cruz <laricruzpires@gmail.com>
2026-09-04 09:58:11 -0300

    Segundo Desafio - Aula 40

---
+++ nível 1/estruturas de repetição/desafiorepeticao2.py
@@ -0,0 +1,8 @@
+# Desafio de Estrutas de Repetição -  FOR
+# Exercício prátrico sobre loops
+
+n = input("Digite um número: ")
+soma = 0
+for i in n:
+    soma += int(i)
+print(f"A soma entre estes números é {soma}.")
