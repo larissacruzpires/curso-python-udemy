1d8ee6daf2873a8b4feca10412fa7403ea546edd
Larissa Cruz <laricruzpires@gmail.com>
2026-09-07 22:46:01 -0300

    Aula 42

--- nível 1/coleções e estruturas de dados/aula42.py
+++ nível 1/coleções e estruturas de dados/aula42.py
@@ -1,16 +1,1 @@
-cidades = []
-
-while True:
-    cidade = input("Digita uma cidade: ")
-    if cidade == 'sair':
-        break
-    else:
-        cidades.append(cidade)
-
-if len(cidades) > 0:
-    cidades.sort()
-
-    for cidade in cidades:
-        print(cidade)
-else:
-    print("A lista de cidades está vazia!")
+# Função Lista
