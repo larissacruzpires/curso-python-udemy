9af27bc47ee0d1f32cca3f4938c04915019ba822
Larissa Cruz <laricruzpires@gmail.com>
2026-09-09 07:31:07 -0300

    Função lista - Cidades

--- nível 1/coleções e estruturas de dados/aula42.py
+++ nível 1/coleções e estruturas de dados/aula42.py
@@ -1,2 +1,18 @@
-# Função Lista (list)
+# Função Lista (list): Cidades
 
+cidades = []
+
+while True:
+    cidade = input("Digite uma cidade: ")
+    if cidade == 'sair':
+        break
+    else:
+        cidades.append(cidade)
+
+if len(cidades) > 0:
+    cidades.sort()
+
+    for cidade in cidades:
+        print(cidade)
+else:
+    print("A lista de cidades está vazia!")
