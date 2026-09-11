271f0e3e46fc4ba85ccc94b5bb5e45fd94ed7693
Larissa Cruz <laricruzpires@gmail.com>
2026-09-10 21:56:46 -0300

    Dicionários - Apartamento

---
+++ nível 1/coleções e estruturas de dados/aula44.py
@@ -0,0 +1,17 @@
+# Dicionários (dict) - Apartamento
+
+proprietarios = {}
+
+while True:
+    apto = int(input('Digite o apto: '))
+    if apto != -1:
+        proprietario = input("Proprietário: ")
+        proprietarios.update({apto:proprietario})
+    else:
+        break
+
+edificio = dict(sorted(proprietarios.items())) # Conversão de list para dict
+
+for chave, valor in  edificio.items():
+    print(f"{chave} - {valor}")
+print(f"Total de apartamentos: {len(edificio)}")
