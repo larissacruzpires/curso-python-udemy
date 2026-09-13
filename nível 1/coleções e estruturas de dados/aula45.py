4ebd0cd849b0ed60b0bc594cd1a2cb9155ccc938
Larissa Cruz <laricruzpires@gmail.com>
2026-09-12 23:41:52 -0300

    Conjuntos

--- nível 1/coleções e estruturas de dados/aula45.py
+++ nível 1/coleções e estruturas de dados/aula45.py
@@ -1,2 +1,17 @@
 # Conjuntos (sets): lista
 
+palavras = []
+lista = []
+
+while True:
+    palavra = input("Digite uma palavra: ")
+    if palavra == '':
+        break
+    else:
+        palavras.append(palavra)
+
+for palavra in palavras:
+    lista.append(list(set(palavra)))
+
+for item in lista:
+    print(item)
